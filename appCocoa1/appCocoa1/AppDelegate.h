8c75c15c2eb9679d58be60fc6eae434fc7227ed5
Francisco Axel Roman Cardoza <47537015+axlRoman@users.noreply.github.com>
2024-02-27 11:08:05 -0600

    Add files via upload

---
+++ appCocoa1/appCocoa1/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  appCocoa1
+//
+//  Created by Axel Roman on 26/02/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ appCocoa1/appCocoa1/AppDelegate.m
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.m
+//  appCocoa1
+//
+//  Created by Axel Roman on 26/02/24.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (strong) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+- (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app {
+    return YES;
+}
+
+
+@end
