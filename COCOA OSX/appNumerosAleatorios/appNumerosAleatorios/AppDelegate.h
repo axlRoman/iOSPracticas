0410b493133df062bae1e96e64859b83478595cc
Francisco AxelRoman Cardoza <franaxel@live.com.mx>
2024-02-28 15:24:07 -0600

    Clase 28 de febrero

---
+++ COCOA OSX/appNumerosAleatorios/appNumerosAleatorios/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  appNumerosAleatorios
+//
+//  Created by Axel Roman on 28/02/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ COCOA OSX/appNumerosAleatorios/appNumerosAleatorios/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  appNumerosAleatorios
+//
+//  Created by Axel Roman on 28/02/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
